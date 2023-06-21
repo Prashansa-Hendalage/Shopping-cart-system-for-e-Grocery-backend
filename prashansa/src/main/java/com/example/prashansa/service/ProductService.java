e011fbf79f3edbe3532cf1533f56c1ede777ad69
Prashansa-Hendalage <134161355+Prashansa-Hendalage@users.noreply.github.com>
2023-06-21 21:45:53 +0530

    first commit

---
+++ prashansa/src/main/java/com/example/prashansa/service/ProductService.java
@@ -0,0 +1,17 @@
+package com.example.prashansa.service;
+
+import com.example.prashansa.dao.ProductDao;
+import com.example.prashansa.entity.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductDao productDao;
+
+    public Product addNewProduct(Product product){
+       return productDao.save(product);
+    }
+}
